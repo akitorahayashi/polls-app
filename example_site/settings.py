134d84429972264f2f3f4987d4ad4043ca5e3f56
akitorahayashi <hayashi20030703@gmail.com>
2025-08-20 17:21:21 +0900

    CIを追加

--- example_site/settings.py
+++ example_site/settings.py
@@ -8,12 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n@x_l@+x%#v@3)g$v*!s@c@p@b@q(z@e@y(f@c@k@e@y"
+SECRET_KEY = "sample-secret-key"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
